f553055f14bb1aadae23d45a8032ef17f91cc340
Leiam Nash <94122613+LeiamNashRebirth@users.noreply.github.com>
2022-11-27 17:10:07 +0800

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,81 @@
+var youtube = "https://www.youtube.com/c/LeiamNash"
+var facebook = "https://www.facebook.com/LeiamNashRebrth";
+var github = "https://github.com/LeiamNashRebirth";
+var messenger = "https://m.facebook.com/messages/read/?tid=cid.c.100050635086635&eav=AfZuoFhevwua5LzfEq2iac2Kcl0emuS9R6fZ91HYWN3qbEs_8cyjox6AAQfTfpZnXws&paipv=0";
+const empty = "&nbsp";
+
+about = [
+  "<br>",
+  "Hello thanks for visiting my website",
+  `I am Leiam Nash, a grade 10 student also im just a 15 years old kid, who enjoys building robots, AI, 
+websites like this one and other cool projects that serve a purpose.`,
+  `I started programing when covid-19 days cause that time im so very bored at my house also i don't have a intention back then to study some programing stuff cause im only a 13 years old back then, the only way that im interested is to donwload a discord app cause you know it's so boring, when im on at discord there's so many bots that i encounter also i have a friend a vietnamese friend he's a developer of a discord bot on the server, one time i message him to give me some of tutorials but he give me a file so i don't know how to use it he didn't also mention me how to use it, because of my curiosity i watch on youtube how to make a bots, how to deploy it and how to scrap and i i learned a lot cause of youtube my first project is a website hahaha also i deleted that website cause it's so embracing also my first language is html... my first messenger bot that i created is when im on 14 years old and yeah that's my story i got addicted to programing stuff`,
+  "<br>",
+];
+
+links = [
+  // format as table to achieve responsive column layout
+  `<table>
+   <tr><td>facebook</td><td><a href="${facebook}" target="_blank">facebook/LeiamNashRebirth</a></td></tr>
+   <tr><td>github</td><td><a href="${github}" target="_blank">github/LeiamNashRebirth</a></td></tr>
+   <tr><td>youtube</td><td><a href="${youtube}" target="_blank">youtube/LeiamNash</a></td></tr>
+   <tr><td>messenger</td><td><a href="${messenger}" target="_blank">facebook/LeiamNashRebirth</a></td></tr>
+   </table>`,
+];
+
+projects = [
+  "<br>",
+  "Work in progress most projects are still offline, on GitHub or confidential <br> <br>Projects that i made: <br>C3C mod <br>Mirai clone <br>Website terminal <br>Api's",
+  "<br>",
+  `<div id="repo-box"></div>`,
+  repos,
+];
+
+help = [
+  "<br>",
+  'Commands List:',
+  // format as table to achieve responsive column layout
+  `<table>
+  <tr><td><span class="command">about</span></td><td>who is leiam nash?</td></tr>
+  <tr><td><span class="command">links</span></td><td>display my links</td></tr>
+  <tr><td><span class="command">projects</span></td><td>view coding projects</td></tr>
+  <tr><td><span class="command">history</span></td><td>view command history</td></tr>
+  <tr><td><span class="command">help</span></td><td>You obviously already know what this does</td></tr>
+  <tr><td><span class="command">messenger</span></td><td>do not message me</td></tr>
+  <tr><td><span class="command">clear</span></td><td>clear terminal</td></tr>
+  <tr><td><span class="command">banner</span></td><td>display the banner</td></tr>
+  <tr><td><span class="command">theme</span></td><td>change the theme</td></tr>
+  <tr><td><span class="command">[tab]</span></td><td>trigger completion</td></tr>
+  </table>`,
+  "<br>",
+  'Also try other commands that you know I may have included some easter eggs 😆',
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  "██╗     ███████╗██╗ █████╗ ███╗   ███╗███╗   ██╗ █████╗ ███████╗██╗  ██╗<br>██║     ██╔════╝██║██╔══██╗████╗ ████║████╗  ██║██╔══██╗██╔════╝██║  ██║<br>██║     █████╗  ██║███████║██╔████╔██║██╔██╗ ██║███████║███████╗███████║<br>██║     ██╔══╝  ██║██╔══██║██║╚██╔╝██║██║╚██╗██║██╔══██║╚════██║██╔══██║<br>███████╗███████╗██║██║  ██║██║ ╚═╝ ██║██║ ╚████║██║  ██║███████║██║  ██║<br>╚══════╝╚══════╝╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                                                        ",
+ "<br>",
+];
+
+welcomeMsg = [
+  '<span class="color2 terminal-welcome-msg">welcome to my personal website</span>',
+  "<span class=\"color2 terminal-welcome-msg\">please type </span> <span class=\"command terminal-welcome-msg\">'help'</span><span class=\"color2 terminal-welcome-msg\"> for commands category</span>",
+  "<br>",
+];
+
+allCommands = [
+  "help", "about", "links", "projects", "messenger", "linkedin", "github", "history", "clear", "banner", "theme",
+  "echo", "ping", "ls", "cd", "vi", "vim", "emacs", "sudo", "gui",
+];
+
+themes = {
+  "coral": "css/style_coral.css",
+  "midnight": "css/style_midnight.css",
+  "chocolate": "css/style_chocolate.css",
+};
+
+allArgs = [
+  "ls", "set", "random",
+];
+
